06cbd5cf746b0a8ab16073a19139319b74777ba8
leslie <guzhenqiang@guzhenqiangs-macbook-pro.local>
2015-12-14 11:27:49 +0800

    Initial Commit

---
+++ textLayerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  textLayerDemo
+//
+//  Created by 顾振强 on 15/12/14.
+//  Copyright © 2015年 leslie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ textLayerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  textLayerDemo
+//
+//  Created by 顾振强 on 15/12/14.
+//  Copyright © 2015年 leslie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
